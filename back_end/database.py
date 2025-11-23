e74721ae313c2d2cc520c498a5af7059a5dfe71f
Felipe S. Nakamura <felipenakamura29@gmail.com>
2025-11-23 02:37:14 -0300

    Terminado Dash front

--- back_end/database.py
+++ back_end/database.py
@@ -2,9 +2,9 @@ import pymysql
 
 def get_db_connection():
     connection = pymysql.connect(
-        host='localhost',       # ou IP do servidor MySQL
-        user='root',     
-        password='123',   
+        host='localhost',
+        user='root',
+        password='1234',
         database='banco_jornada_ademicon',
         cursorclass=pymysql.cursors.DictCursor
     )
